472db2b37e1c60a19dcd3439425552dd21aed36d
Vaibhav4399 <vaibhavprabhu19@gmail.com>
2022-10-23 10:47:46 +0530

    Configured for hosting

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('./dist/weather'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(`index.html`, { root: 'dist/weather' });
+});
+
+app.listen(port);
